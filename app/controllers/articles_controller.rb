f2dbe1f1777f17807af8f77579910bb196363249
EdPaul1 <saidarajab101@gmail.com>
2023-06-28 13:11:16 +0300

    cookies and sessions API

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,17 @@ class ArticlesController < ApplicationController
 
   def show
     article = Article.find(params[:id])
-    render json: article
+
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+
+    if session[:page_views] <= 3
+      # Render JSON response with article data
+      render json: article
+    else
+      # Render JSON response with error message and status code 401 unauthorized
+      render json: { error: "Maximum pageview limit reached"}, status: :unauthorized
+    end
   end
 
   private
